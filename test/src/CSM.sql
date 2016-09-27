98b3940f2064631bb4705050ad43a968d916d687
kp.liu <kuanpeng.liu@ge.com>
2016-09-27 11:37:59 +0900

    commit first change

---
+++ test/src/CSM.sql
@@ -0,0 +1,7 @@
+--ASCSå˚ç¿ïsàÍív
+select TO01GA00,KO04HI24 from sysadm.cam10
+
+--update sysadm.cam10 set KO04HI24='6059074'  where TO01GA00='SSHQPX'
+select KO04HI24 from sysadm.cam10 where TO01GA00='SSHQPX'
+
+--test 
