e84a0c5c82ed335c45f3063b7b17f38c0ba058b8
kp.liu <kuanpeng.liu@ge.com>
2016-09-27 11:53:50 +0900

    commit2

--- test/src/KWbatch/helloworld.java
+++ test/src/KWbatch/helloworld.java
@@ -3,7 +3,9 @@ package KWbatch;
 public class helloworld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub test
+		System.out.println("you are welcome!");
+
 		System.out.println("you are welcome!");
 
 	}
